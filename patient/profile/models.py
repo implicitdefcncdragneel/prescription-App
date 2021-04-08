74c8c57440c59818f35b233a6dadcfb72319cbc5
unknown <47188867+implicitdefcncdragneel@users.noreply.github.com>
2021-04-08 16:06:03 +0530

    file_image_upload_error

---
+++ patient/profile/models.py
@@ -0,0 +1,45 @@
+import uuid
+from django.db import models
+from ..user.models import User
+from django.core.exceptions import ValidationError
+
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    q1= models.CharField(max_length=200,unique=False,default=False)
+    q2= models.CharField(max_length=200,unique=False,default=False)
+    q3= models.CharField(max_length=200,unique=False,default=False)
+    q4= models.CharField(max_length=200,unique=False,default=False)
+    q5= models.CharField(max_length=200,unique=False,default=False)
+
+    docfile = models.FileField(blank=True, default="",upload_to='documents/%Y/%m/%d')
+
+    imgDoc = models.ImageField(blank=True, default="",upload_to='images/')
+
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        '''
+        to set table name in database
+        '''
+        db_table = "profile_patient"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ patient/profile/views.py
@@ -0,0 +1,43 @@
+from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from ..user.serializers import UserRegistrationSerializer
+from .models import UserProfile
+
+
+class UserProfileView(RetrieveAPIView):
+
+    permission_classes = (IsAuthenticated,)
+    authentication_class = JSONWebTokenAuthentication
+
+    def get(self, request):
+        try:
+            user_profile = UserProfile.objects.get(user=request.user)
+            status_code = status.HTTP_200_OK
+            response = {
+                'success': 'true',
+                'status code': status_code,
+                'message': 'User profile fetched successfully',
+                'data': [{
+                    'q1': user_profile.q1,
+                    'q2': user_profile.q2,
+                    'q3': user_profile.q3,
+                    'q4': user_profile.q4,
+                    'q5': user_profile.q5,
+                    'docfile': user_profile.docfile,
+                    'imgDoc':user_profile.imgDoc,
+                    
+                    }]
+                }
+
+        except Exception as e:
+            status_code = status.HTTP_400_BAD_REQUEST
+            response = {
+                'success': 'false',
+                'status code': status.HTTP_400_BAD_REQUEST,
+                'message': 'User does not exists',
+                'error': str(e)
+                }
+        return Response(response, status=status_code)
